e10ae894bd9bdafaf942c6830eb0bf2be4d5287d
Helder <hfernandes238@gmail.com>
2025-06-23 15:28:57 +0200

    Ajout EnableDiscoveryClient

--- src/main/java/com/ff/commandes_service/CommandesServiceApplication.java
+++ src/main/java/com/ff/commandes_service/CommandesServiceApplication.java
@@ -2,10 +2,12 @@ package com.ff.commandes_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class CommandesServiceApplication {
 
 	public static void main(String[] args) {
